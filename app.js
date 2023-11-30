eb373c35abe35e3ea7a31ce9548ad5697f585d3e
Christian Benedict Alzate Quintero <141609433+ChanColossus@users.noreply.github.com>
2023-11-30 12:48:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const portfolio = require('./routes/portfolio');
 
 const order = require('./routes/order');
 
-app.use(cors({
-    origin: ['http://localhost:3000','https://myrmidons.onrender.com']
-}))
+app.use(cors())
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(cookieParser());
